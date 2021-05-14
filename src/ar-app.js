d3807b1c8fac688480946dc74ca55c243d8a9163
gschian0 <54407820+gschian0@users.noreply.github.com>
2021-05-14 18:04:49 -0400

    added files

---
+++ src/ar-app.js
@@ -0,0 +1,71 @@
+import * as THREE from "three";
+const THREEAR = require('threear');
+import marker from "../data/patt.hiro";
+
+var renderer = new THREE.WebGLRenderer({
+  // antialias	: true,
+  alpha: true
+});
+renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+// renderer.setPixelRatio( 2 );
+renderer.setSize( 500, 500 );
+renderer.setPixelRatio(Math.max(window.devicePixelRatio,2))
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.top = '0px'
+renderer.domElement.style.left = '0px'
+document.body.appendChild( renderer.domElement );
+
+// init scene and camera
+var scene = new THREE.Scene();
+var camera = new THREE.Camera();
+scene.add(camera);
+
+var markerGroup = new THREE.Group();
+scene.add(markerGroup);
+
+var source = new THREEAR.Source({ renderer, camera });
+
+THREEAR.initialize({ source: source }).then((controller) => {
+  console.log(controller);
+  // add a torus knot		
+  var geometry = new THREE.TorusKnotGeometry(0.3,0.1,64,16);
+  var material = new THREE.MeshNormalMaterial(); 
+  var torus = new THREE.Mesh( geometry, material );
+  torus.position.y = 0.5
+  markerGroup.add(torus);
+
+  var geometry = new THREE.BoxBufferGeometry(1,1,1);
+  var material = new THREE.MeshNormalMaterial({
+    transparent : true,
+    opacity: 0.5,
+    side: THREE.DoubleSide
+  }); 
+  var cube = new THREE.Mesh( geometry, material );
+  cube.position.y	= geometry.parameters.height / 2;
+  markerGroup.add(cube)
+
+  var patternMarker = new THREEAR.PatternMarker({
+    patternUrl: marker,
+    markerObject: markerGroup
+  });
+
+  controller.trackMarker(patternMarker);
+
+  // run the rendering loop
+  var lastTimeMsec = 0;
+  requestAnimationFrame(function animate(nowMsec){
+    // keep looping
+    requestAnimationFrame( animate );
+    // measure time
+    lastTimeMsec = lastTimeMsec || nowMsec-1000/60;
+    var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+    lastTimeMsec = nowMsec;
+    // call each update function
+    controller.update( source.domElement );
+    // cube.rotation.x += deltaMsec/10000 * Math.PI
+    torus.rotation.y += deltaMsec/1000 * Math.PI
+    torus.rotation.z += deltaMsec/1000 * Math.PI
+    renderer.render( scene, camera );
+  });
+
+});
